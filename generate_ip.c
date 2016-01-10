e68fa004f03ef7d49a61e0733b2ea18618dae6c9
Wangzi <756651720@qq.com>
2016-01-10 05:49:30 -0800

    add generate

---
+++ generate_ip.c
@@ -0,0 +1,55 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+/*
+  本程序用来生产随机的ip地址。
+  传入缓冲区的指针即可。
+*/
+
+
+int generate_ip(char *ip){
+  
+  srand((unsigned)time(NULL));
+  
+  int ip1 = rand()%256+1;
+  int ip2 = rand()%256+1;
+  int ip3 = rand()%256+1;
+  int ip4 = rand()%256+1;
+  
+  //char ip[50];
+  char ip1_[50];
+  sprintf(ip1_, "%d", ip1);
+  char ip2_[50];
+  sprintf(ip2_, "%d", ip2);
+  char ip3_[50];
+  sprintf(ip3_, "%d", ip3);
+  char ip4_[50];
+  sprintf(ip4_, "%d", ip4);
+
+  printf("%s\n", ip1_);
+  const char c[10] = ".";
+  strcpy(ip, ip1_);
+  strcat(ip, c);
+  strcat(ip, ip2_);
+  strcat(ip, c);
+  strcat(ip, ip3_);
+  strcat(ip, c);
+  strcat(ip, ip4_);
+  
+  //printf("%s\n", ip);
+  return 0;
+}
+
+int main(){
+  
+  char ip[50];
+  generate_ip(ip);
+  
+  printf("%s\n", ip);
+  return 0;
+  
+}
